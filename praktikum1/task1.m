fa3b2f25084c23f9d25ecf1c5bde04a834deeee3
Oliver Erxleben <oliver.erxleben@me.com>
2016-03-19 10:07:43 +0100

    added tasks b, c, d

--- praktikum1/task1.m
+++ praktikum1/task1.m
@@ -35,4 +35,63 @@ umkA = A^(-1);
 x = v \ A;
 
 %% 1b
-A
+% erste und letzte Spalte aus A addieren
+disp(A(:,1));
+disp(A(:, end));
+A_1_end_add = A(:,1) + A(:,end);
+
+% zweite und letzte Zeile entfernen und Produkt 
+A_orig = A;
+A(2,:) = [];
+A_t = A;
+
+A(:,end) = [];
+A_t = A;
+
+At_A = A * A_t;
+
+% Spalten ersetzen
+A = A_orig;
+A(3,:) = [1 2 3 4];
+A(:,2) = [5; 7; 9; 11];
+disp(A_orig);
+disp(A);
+
+% Spalten und Zeilen erweitern
+A = A_orig;
+disp(A);
+
+% Spalte mit 1en 
+A = [A,ones(4,1)];
+% Spalte mit 2en
+% TODO: length() Funktion benutzen
+twos = [2;2;2;2];
+A = [A,twos];
+disp(A);
+
+%% 1c
+A = A_orig;
+disp(A);
+ij = find(A<0);
+disp(ij);
+negValsA = A(A<0);
+disp(negValsA);
+
+%% 1d
+% sin in -4 bis 4
+x = [-4:0.1:4]; % startval : increment : endval // increment approx 0 gives best figure 
+y = sin(x);
+plot(x,y)
+
+% 3 func in one plot
+y_sin = sin(x);
+y_cos = cos(x);
+y_add = sin(x) + cos(x);
+figure
+plot(x,y_sin, '-g');
+hold on
+plot(x, y_cos, '-r');
+hold on
+plot(x, y_add, '-b');
+print('task1d', '-djpeg'); % save as jpg on disk
+hold off
