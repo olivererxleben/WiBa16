d5852c06da8531f2950c2cdb2b6d14f0403f4dc3
Oliver Erxleben <oliver.erxleben@me.com>
2016-03-26 22:07:01 +0100

    apriori init commit

---
+++ praktikum2/Apriori.m
@@ -0,0 +1,13 @@
+classdef Apriori
+    %APRIORI Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+       
+    end
+    
+end
+
